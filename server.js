291eaab25cadc42f61df4462943fb8cf17406d2c
Dev.Nah <mcthundercat99@gmail.com>
2022-04-26 11:13:03 -0400

    Added Heroku

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const { animals } = require('./data/animals');
+
+// Filters the query
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    let filteredResults = animalsArray;
+    if (query.personalityTraits) {
+        if (typeof query.personalityTraits === 'string') {
+            personalityTraitsArray = [query.personalityTraits];
+        } else {
+            personalityTraitsArray = query.personalityTraits;
+        }
+        personalityTraitsArray.forEach (trait => {
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
+        });
+    };
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+}
+
+
+// Adds the route
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+})
+
+// Adds the port
+app.listen(441, () => {
+    console.log('Server is running on port 441');
+})
